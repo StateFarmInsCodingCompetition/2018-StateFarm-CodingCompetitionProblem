4353db92f50237f990a426509330f9e2936be0d0
Dill <dillvani@me.com>
2018-10-14 00:48:50 -0500

    Submission

--- src/main/java/com/codingcompetition/statefarm/StreetMapDataInterpreter.java
+++ src/main/java/com/codingcompetition/statefarm/StreetMapDataInterpreter.java
@@ -2,28 +2,64 @@ package com.codingcompetition.statefarm;
 
 import com.codingcompetition.statefarm.model.PointOfInterest;
 import com.codingcompetition.statefarm.utility.PointOfInterestParser;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreetMapDataInterpreter implements Interpreter {
-
+	
+	String fileToRead;
 
     public StreetMapDataInterpreter(String s) {
+    	fileToRead = s;
     }
 
     @Override
     public List<PointOfInterest> interpret() {
-        return null;
+    	try {
+    		String filepath = fileToRead;
+    		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+    		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+    		Document doc = docBuilder.parse(filepath);
+
+    		NodeList list = doc.getElementsByTagName("node");
+
+    		System.out.println("Total of elements : " + list.getLength());
+    		
+    		System.out.println(list.equals(fileToRead));
+    		
+    		for (int i = 0; i < list.getLength(); i++) {
+    			
+    		}
+    		
+    	} catch (ParserConfigurationException pce) {
+    		pce.printStackTrace();
+    	} catch (IOException ioe) {
+    		ioe.printStackTrace();
+    	} catch (SAXException sae) {
+    		sae.printStackTrace();
+    	}
+    	
+		return null;
     }
 
     @Override
     public List<PointOfInterest> interpret(SearchCriteria criteria) {
+    	if (criteria == null) {
+    		return Collections.emptyList();
+    	}
+    	
         return null;
     }
 
--- src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
+++ src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
@@ -1,6 +1,8 @@
 package com.codingcompetition.statefarm.utility;
 
 import com.codingcompetition.statefarm.model.PointOfInterest;
+import com.sun.javafx.scene.paint.GradientUtils.Parser;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -16,14 +18,49 @@ import java.util.Stack;
 
 public class PointOfInterestParser {
 
-
     // Stacks for storing the elements and objects.
     private Stack<String> elements = new Stack<String>();
     private Stack<PointOfInterest> objects = new Stack<PointOfInterest>();
 
-
     public List<PointOfInterest> parse(String fileName) throws IOException, SAXException {
-   return null;
-    }
+    	System.out.println();
+    	try {
+    		SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+        	
+        	SAXParser parser = parserFactory.newSAXParser();
 
+        	DefaultHandler handler = new DefaultHandler() {
+        		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {         		
+            		if (qName.equalsIgnoreCase("NODE")) {
+            			String lat = attributes.getValue("lat");
+            			elements.push(lat);
+            			String lon = attributes.getValue("lon");
+            			elements.push(lon);
+            			
+            			objects.add(new PointOfInterest());
+            		} 
+            		
+            		if (qName.equalsIgnoreCase("TAGS")) {
+            			String k = attributes.getValue("k");
+            			elements.push(k);
+            			String v = attributes.getValue("v");
+            			elements.push(v);
+            			
+            		}
+            	}
+        		
+        		@SuppressWarnings("unused")
+				public void lastElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            		System.out.println("Last element: " + qName);
+            	}
+        	};
+        	
+        	parser.parse(fileName, handler);
+        	
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	
+		return objects;	
+    }
 }
