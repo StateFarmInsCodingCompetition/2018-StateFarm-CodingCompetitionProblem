9adc119e2bd19d784fb3c9b9ab7d0533d092dd45
Aashish Welling <aashishwelling@gmail.com>
2018-10-13 22:09:44 -0400

    new

--- src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
+++ src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
@@ -6,14 +6,12 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.*;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class PointOfInterestParser {
@@ -37,7 +35,8 @@ public class PointOfInterestParser {
         Document d = builder.parse(getClass().getResource(fileName).getFile());
         NodeList meta = d.getElementsByTagName("node");
         List<PointOfInterest> poi = new ArrayList<>();
-        for(int i = 0; i < meta.getLength(); i++) {
+        int len = meta.getLength();
+        for(int i = 0; i < len; i++) {
             Node n = meta.item(i);
             poi.add(new PointOfInterest(n));
         }
