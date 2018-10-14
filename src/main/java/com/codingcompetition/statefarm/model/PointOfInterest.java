659f5fc64b82dc3c5d3d8449da21c320d54646c1
Harshal Sheth <hsheth2@gmail.com>
2018-10-13 23:45:04 -0400

    Add javadoc and code cleanup

--- src/main/java/com/codingcompetition/statefarm/model/PointOfInterest.java
+++ src/main/java/com/codingcompetition/statefarm/model/PointOfInterest.java
@@ -5,23 +5,52 @@ import java.util.Map;
 
 public class PointOfInterest {
 
+    /**
+     * The latitute of this point of interest.
+     */
     private final String latitude;
+
+    /**
+     * The longitute of this point of interest.
+     */
     private final String longitude;
+
+    /**
+     * A map containing a list of descriptors corresponding
+     * to this point of interest.
+     */
     private final Map<Object, String> descriptors = new HashMap<>();
 
+    /**
+     * Construct a point of interest.
+     * @param latitude the latitude of this point of interest
+     * @param longitude the longituge of this point of interest
+     */
     public PointOfInterest(String latitude, String longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
+    /**
+     * Fetches the current list of descriptors for this point of interest.
+     * @return the list of descriptors
+     */
     public Map<Object,String> getDescriptors() {
         return descriptors;
     }
 
+    /**
+     * Get the latitude.
+     * @return the latitude
+     */
     public String getLatitude() {
         return latitude;
     }
 
+    /**
+     * Get the longitude.
+     * @return the longitude
+     */
     public String getLongitude() {
         return longitude;
     }
--- src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
+++ src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
@@ -15,40 +15,62 @@ import java.util.Stack;
 
 public class PointOfInterestParser {
 
-
     // Stacks for storing the elements and objects.
     private Stack<String> elements = new Stack<String>();
     private Stack<PointOfInterest> objects = new Stack<PointOfInterest>();
 
+    // Handler for parsing
+    private DefaultHandler saxHandler = new DefaultHandler() {
+        @Override
+        public void startDocument() throws SAXException {
+            elements.clear();
+        }
 
+        @Override
+        public void endDocument() throws SAXException {
+            if (elements.size() != 0)
+                throw new SAXException("untermintated tags exist");
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            if (qName.equalsIgnoreCase("node")) {
+                String lat = attributes.getValue("lat");
+                String lon = attributes.getValue("lon");
+                PointOfInterest poi = new PointOfInterest(lat, lon);
+
+                objects.push(poi);
+            } else if (qName.equalsIgnoreCase("tag")) {
+                if (elements.peek().equalsIgnoreCase("node")) {
+                    PointOfInterest poi = objects.peek();
+                    poi.getDescriptors().put(attributes.getValue("k"), attributes.getValue("v"));
+                }
+            }
+
+            elements.push(qName);
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if (!elements.peek().equalsIgnoreCase(qName)) {
+                throw new SAXException("unmatched tag: " + qName);
+            }
+            elements.pop();
+        }
+    };
+
+    /**
+     * Read a list of points of interested from a file.
+     * @param fileName the name of the file to read from
+     * @return a list of the points of interest contained within the referenced file
+     */
     public List<PointOfInterest> parse(String fileName) throws IOException, SAXException, ParserConfigurationException {
         SAXParserFactory factory = SAXParserFactory.newInstance();
         factory.setValidating(true);
         SAXParser saxParser = factory.newSAXParser();
-        saxParser.parse(this.getClass().getResourceAsStream(fileName), new DefaultHandler() {
-            @Override
-            public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-                if (qName.equalsIgnoreCase("node")) {
-                    objects.push(new PointOfInterest(attributes.getValue("lat"), attributes.getValue("lon")));
-                } else if (qName.equalsIgnoreCase("tag")) {
-                    PointOfInterest poi = objects.peek();
-                    poi.getDescriptors().put(attributes.getValue("k"), attributes.getValue("v"));
-                }
-//                super.startElement(uri, localName, qName, attributes);
-            }
+        saxParser.parse(this.getClass().getResourceAsStream(fileName), saxHandler);
 
-            @Override
-            public void endElement(String uri, String localName, String qName) throws SAXException {
-                super.endElement(uri, localName, qName);
-            }
-
-            @Override
-            public void characters(char[] ch, int start, int length) throws SAXException {
-                super.characters(ch, start, length);
-            }
-        });
-
-        return objects;
+        return new ArrayList<>(objects);
     }
 
 }
