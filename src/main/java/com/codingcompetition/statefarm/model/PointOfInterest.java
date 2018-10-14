3ed20b3fbd727706644c3ba4285fc841da23267d
Amaan Marfatia <amaanmarfatia@gmail.com>
2018-10-13 23:47:16 -0400

    Pass PointOfInterestParser

--- src/main/java/com/codingcompetition/statefarm/model/PointOfInterest.java
+++ src/main/java/com/codingcompetition/statefarm/model/PointOfInterest.java
@@ -1,19 +1,24 @@
 package com.codingcompetition.statefarm.model;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class PointOfInterest {
 
+    final String lat = "lat";
+    final String lon = "lon";
+    private Map<Object, String> descriptors;
+
+    public PointOfInterest(Map<Object, String> descriptors) {
+        this.descriptors = descriptors;
+    }
+
     public Map<Object,String> getDescriptors() {
-    return new HashMap<>();
+        return descriptors;
     }
 
     public String getLatitude() {
-        return "";
+        return descriptors.get(lat);
     }
 
-    public String getLongitude() {
-        return "";
-    }
+    public String getLongitude() { return descriptors.get(lon); }
 }
--- src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
+++ src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
@@ -8,10 +8,13 @@ import org.xml.sax.helpers.DefaultHandler;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
 public class PointOfInterestParser {
@@ -22,8 +25,63 @@ public class PointOfInterestParser {
     private Stack<PointOfInterest> objects = new Stack<PointOfInterest>();
 
 
-    public List<PointOfInterest> parse(String fileName) throws IOException, SAXException {
-   return null;
+    public List<PointOfInterest> parse(String fileName) throws IOException {
+        BufferedReader bf = new BufferedReader(new FileReader("src\\main\\resources\\" + fileName));
+        String line;
+        int count = 0;
+        List<PointOfInterest> list = new ArrayList<>();
+        while ((line = bf.readLine()) != null) {
+            int beginNodeIndex = line.indexOf("<node") + "<node ".length();
+            if (beginNodeIndex >= "<node ".length()) {
+                // read all the things and make a point of interest
+                Map<Object, String> map = new HashMap<>();
+                line = line.substring(beginNodeIndex);
+                while (line.length() > 2) {
+                    String key = parseKey(line);
+                    String value = parseValue(line);
+                    if (key != null && value != null) {
+                        map.put(key, value);
+                    }
+                    int firstValueIndex = line.indexOf("\" ");
+                    line = line.substring((firstValueIndex >= 0) ? firstValueIndex + 2 : line.lastIndexOf('"') + 1);
+                }
+
+                // Add all tags to map
+                if (!line.contains("/>")) {
+                    while (!(line = bf.readLine().trim()).contains("</node>")) {
+                        if (line.contains("<tag")) {
+                            int firstValueIndex = line.indexOf("=\"");
+                            line = line.substring(line.lastIndexOf(' ', firstValueIndex) + 1);
+                            String key = parseKey(line);
+                            firstValueIndex = line.indexOf("=\"", firstValueIndex + 1);
+                            line = line.substring(line.lastIndexOf(' ', firstValueIndex) + 1);
+                            String value = parseValue(line);
+                            if (key != null && value != null) {
+                                map.put(key, value);
+                            }
+                        }
+                    }
+                }
+                list.add(new PointOfInterest(map));
+            }
+        }
+        return list;
+    }
+
+    private String parseKey(String s) {
+        int keyEndIndex = s.indexOf('=');
+        if (keyEndIndex < 0) {
+            return null;
+        }
+        return s.substring(0, keyEndIndex);
+    }
+
+    private String parseValue(String s) {
+        int valueBegin = s.indexOf('"') + 1;
+        if (valueBegin < 1) {
+            return null;
+        }
+        return s.substring(valueBegin, s.indexOf('"', valueBegin));
     }
 
 }
