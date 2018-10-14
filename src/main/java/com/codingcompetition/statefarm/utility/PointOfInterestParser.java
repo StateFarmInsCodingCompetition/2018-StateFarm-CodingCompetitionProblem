e45a0e9013828f4c642c9545d5a3b31ede6fd07b
Abdul <abdulbagasra@gmail.com>
2018-10-13 20:40:21 -0500

    Continue parsing

--- src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
+++ src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
@@ -2,6 +2,8 @@ package com.codingcompetition.statefarm.utility;
 
 import com.codingcompetition.statefarm.model.PointOfInterest;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -26,7 +28,7 @@ public class PointOfInterestParser {
     public List<PointOfInterest> parse(String fileName) throws IOException, SAXException, ParserConfigurationException {
         List<PointOfInterest> result = new ArrayList<>();
         ClassLoader classLoader = getClass().getClassLoader();
-        File inputFile = new File(classLoader.getResource(fileName).getFile());
+        File inputFile = new File(classLoader.getResource(fileName.substring(1)).getFile());
 
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -34,6 +36,13 @@ public class PointOfInterestParser {
         doc.getDocumentElement().normalize();
 
         NodeList nList = doc.getElementsByTagName("meta");
+        for (int i = 0; i < nList.getLength(); i++) {
+          Node node = nList.item(i);
+          if (!node.getNodeName().equals("node")) {
+            continue;
+          }
+          NamedNodeMap attributeMap = node.getAttributes();
+        }
 
         return null;
     }
