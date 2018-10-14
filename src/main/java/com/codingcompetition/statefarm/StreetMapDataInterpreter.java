d544d13d7715ea283956d280b5823d11e5371e20
Olivia Phillips <olphil99@gmail.com>
2018-10-13 23:48:23 -0500

    tried criteria

--- src/main/java/com/codingcompetition/statefarm/StreetMapDataInterpreter.java
+++ src/main/java/com/codingcompetition/statefarm/StreetMapDataInterpreter.java
@@ -1,8 +1,10 @@
 package com.codingcompetition.statefarm;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.xml.sax.SAXException;
 
@@ -10,7 +12,7 @@ import com.codingcompetition.statefarm.model.PointOfInterest;
 import com.codingcompetition.statefarm.utility.PointOfInterestParser;
 
 public class StreetMapDataInterpreter implements Interpreter {
-	
+
 	private List<PointOfInterest> poiList;
 
 	public StreetMapDataInterpreter(String s) throws IOException, SAXException {
@@ -24,7 +26,20 @@ public class StreetMapDataInterpreter implements Interpreter {
 
 	@Override
 	public List<PointOfInterest> interpret(SearchCriteria criteria) {
-		return null;
+		
+		List<PointOfInterest> results = new ArrayList<>();
+		
+		for(PointOfInterest poi : poiList) {
+			if (poi.getDescriptors().keySet().contains(criteria.getCategory().toString().toLowerCase())) {
+				
+				System.out.println(poi.getDescriptors().get(criteria.getCategory().toString().toLowerCase()));
+				if (poi.getDescriptors().get(criteria.getCategory().toString().toLowerCase()).equalsIgnoreCase(criteria.getValue())) {
+					results.add(poi);
+				}
+			}
+		}
+
+		return results;
 	}
 
 	@Override
