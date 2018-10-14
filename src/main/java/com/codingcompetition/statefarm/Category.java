e45e4f6489ee534beb50b2a6e952e8084cefa47a
Abdul <abdulbagasra@gmail.com>
2018-10-13 22:54:48 -0500

    Refactor Category, SearchCriteria, PointOfInterest, and PointOfInterestParser

--- src/main/java/com/codingcompetition/statefarm/Category.java
+++ src/main/java/com/codingcompetition/statefarm/Category.java
@@ -1,14 +1,17 @@
 package com.codingcompetition.statefarm;
 
-import com.codingcompetition.statefarm.model.PointOfInterest;
-
-import java.util.function.BiFunction;
-
-import static com.codingcompetition.statefarm.MatchingStrategy.ENDS_WITH;
-import static com.codingcompetition.statefarm.MatchingStrategy.EXACT_MATCH;
-import static com.codingcompetition.statefarm.MatchingStrategy.STARTS_WITH;
+import java.util.Arrays;
 
 public enum Category {
+  LEISURE, NAME, AMENITY, CUISINE, SHOP, WHEELCHAIR, HIGHWAY, PLACE, POPULATION, POWER, BUILDING, BEAUTY, NAMESTARTSWITH, NAMEENDSWITH;
 
-    LEISURE, NAME, AMENITY, CUISINE, SHOP, WHEELCHAIR, HIGHWAY, PLACE, POPULATION, POWER, BUILDING, BEAUTY, NAMESTARTSWITH, NAMEENDSWITH;
+  /**
+   * Returns true if the given string represents a valid category.
+   *
+   * @param categoryStr String that may represent a Category
+   * @return true if given string represents valid category, false otherwise
+   */
+  public static boolean isValidCategory(String categoryStr) {
+    return Arrays.stream(Category.values()).anyMatch((category) -> category.name().equalsIgnoreCase(categoryStr));
+  }
 }
