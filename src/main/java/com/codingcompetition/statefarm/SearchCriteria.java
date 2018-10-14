b7267b5eb6be45f82df6d7e3e3e16414950a12fc
K <kiettran8795@gmail.com>
2018-10-13 23:38:18 -0400

    modified streemapdatainterpreter

--- src/main/java/com/codingcompetition/statefarm/SearchCriteria.java
+++ src/main/java/com/codingcompetition/statefarm/SearchCriteria.java
@@ -2,7 +2,19 @@ package com.codingcompetition.statefarm;
 
 public class SearchCriteria {
 
-
+	private Category _cat;
+	private String _value;
+	
 	public SearchCriteria(Category cat, String value) {
+		_cat = cat;
+		_value = value;
+	}
+	
+	public Category getCategory() {
+		return _cat;
+	}
+	
+	public String getValue() {
+		return _value;
 	}
 }
